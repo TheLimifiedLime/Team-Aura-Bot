78142b20813a055268c50332b92960dd092c74de
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2021-06-25 01:46:49 +0000

    Add logging for several shard events

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { log } from "./functions/logger.js"
+import { log } from "./functions/logger.js";
 import { ShardingManager } from "discord.js";
 // Create a new ShardingManager
 const manager = new ShardingManager("./dist/bot.js", {
@@ -6,8 +6,17 @@ const manager = new ShardingManager("./dist/bot.js", {
 });
 
 // Spawn the shards
-manager.spawn()
+manager.spawn();
 
 manager.on("shardCreate", (shard) => {
-  log(`Shard #${shard.id} Launched`, "shard")
-})
+  log(`Shard #${shard.id} Launched`, "shard");
+  shard.on("death", () => {
+    log(`Shard #{shard.id} died`, "error")
+  })
+  shard.on("error", (error) => {
+    log(`Shard #${shard.id} experienced an error`, "error");
+  });
+  shard.on("disconnect", () => {
+    log(`Shard #${shard.id} was disconnected`,"error")
+  })
+});
