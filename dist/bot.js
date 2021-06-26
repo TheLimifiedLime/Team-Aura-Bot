154d31931f4fa077a157bcf7902d168013497c95
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2021-06-26 02:15:24 +0000

    Register command groups and default commands

--- dist/bot.js
+++ dist/bot.js
@@ -7,5 +7,11 @@ var discord_js_commando_1 = require("discord.js-commando");
 var config_json_1 = __importDefault(require("./config.json"));
 var bot = new discord_js_commando_1.CommandoClient({
     commandPrefix: "a?",
-    owner: config_json_1.default.ownerIDS
+    owner: config_json_1.default.ownerIDS,
 });
+bot.registry
+    .registerDefaultTypes()
+    .registerGroups([["owner", "Owner"]])
+    .registerDefaultGroups()
+    .registerDefaultCommands()
+    .registerCommandsIn(__dirname + "/commands");
--- src/bot.ts
+++ src/bot.ts
@@ -1,6 +1,13 @@
 import { CommandoClient } from "discord.js-commando";
-import config from "./config.json"
+import config from "./config.json";
 const bot = new CommandoClient({
-    commandPrefix: "a?",
-    owner: config.ownerIDS
-})
+  commandPrefix: "a?",
+  owner: config.ownerIDS,
+});
+
+bot.registry
+  .registerDefaultTypes()
+  .registerGroups([["owner", "Owner"]])
+  .registerDefaultGroups()
+  .registerDefaultCommands()
+  .registerCommandsIn(__dirname + "/commands");
