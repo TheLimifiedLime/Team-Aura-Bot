0fb2f121554f6611bcac00a237aadb1ec6815b4c
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2021-06-26 02:04:47 +0000

    Setup CommandoClient

--- dist/bot.js
+++ dist/bot.js
@@ -1,2 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+var discord_js_commando_1 = require("discord.js-commando");
+var config_json_1 = __importDefault(require("./config.json"));
+var bot = new discord_js_commando_1.CommandoClient({
+    commandPrefix: "a?",
+    owner: config_json_1.default.ownerIDS
+});
--- src/bot.ts
+++ src/bot.ts
@@ -1,1 +1,6 @@
 import { CommandoClient } from "discord.js-commando";
+import config from "./config.json"
+const bot = new CommandoClient({
+    commandPrefix: "a?",
+    owner: config.ownerIDS
+})
