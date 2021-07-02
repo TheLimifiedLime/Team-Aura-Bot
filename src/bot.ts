c373ee7ee8c46fb50490f4768fa41c57e87dc8df
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2021-07-02 01:17:31 -0500

    Setup basic client

--- src/bot.ts
+++ src/bot.ts
@@ -1,3 +1,10 @@
 const { Client } = require("discord.js")
 const config = require("./config.json");
 const { log } = require("./functions/logger.js")
+const bot = new Client()
+
+bot.on('ready', async () => {
+  log(`Logged in as ${bot.user.tag}`)
+})
+
+bot.login()
