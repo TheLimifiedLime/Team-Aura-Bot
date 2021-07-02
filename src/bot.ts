2f08dc7cb43e9ae92617e6c75190c642681dd1dc
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2021-07-02 01:02:44 -0500

    Remove old commando stuff

--- src/bot.ts
+++ src/bot.ts
@@ -1,26 +1,3 @@
 const { Client } = require("discord.js")
 const config = require("./config.json");
 const { log } = require("./functions/logger.js")
-const bot = new Client({
-  commandPrefix: config.prefix,
-  owner: config.ownerIDS,
-});
-
-bot.registry
-  .registerDefaultTypes()
-  .registerGroups([["owner", "Owner"]])
-  .registerDefaultGroups()
-  .registerDefaultCommands()
-  .registerCommandsIn(__dirname + "/commands");
-
-bot.on("ready", () => {
-    // Done so typescript shuts the fuck up
-    log(`Logged in as ${bot.user!.tag}`, "success");
-    bot.user!.setActivity("you sleep", {
-        type: "WATCHING"
-    })
-});
-
-bot.on("error", console.error);
-
-bot.login()
